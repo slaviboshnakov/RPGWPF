b5c5f89dbbb2d1d12e7e0c466e15b870db20211c
Slavi Boshnakov <43034168+slaviboshnakov@users.noreply.github.com>
2020-09-23 19:23:02 +0800

    04.1
    Created the Location class, added an object of type Location to the GameSession.

---
+++ RPG/Engine/Models/Location.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Models
+{
+   public class Location
+    {
+        public int xCoordinate { get; set; }
+        public int yCoordinate { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageName { get; set; }
+
+
+
+    }
+}
---
+++ RPG/Engine/Viewmodels/GameSession.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Engine.Models;
+
+
+namespace Engine.Viewmodels
+{
+    public class GameSession
+    {
+        public Engine.Models.player CurrentPlayer { get; set; }
+        public Engine.Models.Location CurrentLocation {get; set;}
+
+        public GameSession(){
+
+            CurrentPlayer = new Engine.Models.player();
+            CurrentPlayer.Name = "Slavi";
+            CurrentPlayer.Gold = 1000000;
+            CurrentPlayer.CharacterClass = "Fighter";
+            CurrentPlayer.HitPoints = 10;
+            CurrentPlayer.ExperiencePoints = 0;
+            CurrentPlayer.Level = 1;
+
+            CurrentLocation = new Location();
+            CurrentLocation.Name = "Home";
+            CurrentLocation.xCoordinate = 0;
+            CurrentLocation.yCoordinate = -1;
+            CurrentLocation.Description = "Home sweet home!";
+            CurrentLocation.ImageName = "/Engine;component/Images/Locations/Home.png";
+
+        }
+    }
+}
